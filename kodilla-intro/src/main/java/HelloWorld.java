75d68832fa93c896c935d7f0e0a8965781ed4faf
Michal Malecki <michal.mal.poczta@gmail.com>
2022-12-17 23:35:19 +0100

    HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Michal");
+        System.out.println(28);
+        System.out.println('M');
+
+    }
+}
