c000f2e3e9c3346531bc90f29fb185d7bc5bbf1b
Michal Malecki <michal.mal.poczta@gmail.com>
2022-12-19 20:08:46 +0100

    LeapYear

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,13 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int givenYear = 2400;
+
+        System.out.println(givenYear);
+
+        if (givenYear % 4 == 0 && givenYear % 100 != 0 || givenYear % 400 == 0) {
+            System.out.println("Rok przestepny");
+        } else {
+            System.out.println("Nie jest to rok przestepny");
+        }
+    }
+}
