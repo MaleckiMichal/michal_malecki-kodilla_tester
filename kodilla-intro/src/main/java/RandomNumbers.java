2e66c45d4dfb80b04be5d07e111d2b857fcb2809
Michal Malecki <michal.mal.poczta@gmail.com>
2023-01-15 22:02:10 +0100

    Task_2.5_corrected

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,3 +1,55 @@
+import java.util.Random;
+
+public class RandomNumbers {
+    private int min;
+    private int max;
+
+    public void draw() {
+        Random random = new Random();
+        int sum = 0;
+        this.min = 30;
+        this.max = 0;
+        while (sum < 5000) {
+            int currentNumber = random.nextInt(31);
+            sum += currentNumber;
+
+            System.out.println("num: "+currentNumber+" sum: "+sum);
+
+            if (currentNumber <= this.min) {
+                this.min = currentNumber;
+            }
+            if (currentNumber >= this.max) {
+                this.max = currentNumber;
+            }
+        }
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public static void main(String[] args) {
+        RandomNumbers randomNumbers = new RandomNumbers();
+        randomNumbers.draw();
+        System.out.println("Max: " + randomNumbers.getMax());
+        System.out.println("Min: " + randomNumbers.getMin());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*
 import java.util.Arrays;
 import java.util.Random;
 
@@ -44,3 +96,5 @@ public class RandomNumbers {
         System.out.println(Arrays.toString(arr));
     }
 }
+
+ */
