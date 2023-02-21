ea47e87f9068fc5abe2cf02085d41fe51be94aee
Michal Malecki <michal.mal.poczta@gmail.com>
2023-02-21 12:11:05 +0100

    Task_6.5_revised

--- kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -2,6 +2,8 @@ package com.kodilla.exception.homework;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WarehouseTestSuite {
@@ -16,5 +18,18 @@ class WarehouseTestSuite {
         // when
         // then
         assertThrows(OrderDoesntExistException.class, () -> warehouse.getOrder("0157M"));
+    }
+
+    @Test
+    public void testGetOrder() throws OrderDoesntExistException {
+        // given
+        Warehouse warehouse = new Warehouse();
+        warehouse.addOrder(new Order("0154M"));
+        warehouse.addOrder(new Order("0155M"));
+        warehouse.addOrder(new Order("0156M"));
+        Order expected = new Order("0155M");
+        // when
+        // then
+        assertEquals(expected, warehouse.getOrder("0155M"));
     }
 }
